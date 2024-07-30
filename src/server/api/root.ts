8771da0e22be0050b763b21a9102517dd9ee4596
Utopi-a <bridgestone.amatsu0228@gmail.com>
2024-07-30 16:40:52 +0900

    fix: 余計なファイルを削除した。

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { pixivRouter } from "./routers/pixiv";
 
@@ -8,7 +7,6 @@ import { pixivRouter } from "./routers/pixiv";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   pixiv: pixivRouter,
 });
 
